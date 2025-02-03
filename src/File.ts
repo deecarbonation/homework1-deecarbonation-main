31ac431e65c4b4c4438deb0f39308c1db67c8838
Darien <143901924+deecarbonation@users.noreply.github.com>
2025-02-02 23:09:46 -0600

    Passing 25/50 tests now, only 2 dust bunnies presented with lint

--- src/File.ts
+++ src/File.ts
@@ -6,7 +6,7 @@ export class EOFError extends Error {
     }
 }
 
-// TODO IllegalStateError
+// IllegalStateError
 export class IllegalStateError extends Error {
     constructor(message : string) {
       super(message); //sends 'message' to the parent Error constructor
@@ -14,27 +14,29 @@ export class IllegalStateError extends Error {
       this.name = 'IllegalStateError';
     }
 }
-// TODO: FileMode
+// FileMode
 export enum FileMode {
-Read = 0,
-Write = 1 << 0,
-ReadWrite = 1 << 1,
-Append = 1 << 2,
-WriteAppend = 1 << 3,
-ReadWriteAppend = 1 << 4
+Read = 0,       // 0
+Write = 1 << 0,     //1
+ReadWrite = 1 << 1,   //2
+Append = 1 << 2,      //4
+WriteAppend = 1 << 3,    //8
+ReadWriteAppend = 1 << 4   //16
 } //how is bitwise manipulation important to the solution i.e. 1 << 2
-
-// TODO: FileHandle
+  //answer: ohhhhh it lets you type the decimal of the assigned binary number?
+  
+// FileHandle
 export interface FileHandle {
   read(num: number): Uint8Array; //read at most given number of bytes from file. Throw EOFError if no bytes are left. Returns Uint8Array of bytes read.
 
-  write(array: Uint8Array): void; //writes all bytes in array to file;
+  write(data: Uint8Array): void; //writes all bytes in array to file;
 
   truncate(): void; //ends file at current point. only allowed in "write" mode.
 
   seek(num: number): void; // moves point where reading/writing to the point given within file. throw RangeError if number is negative or beyond file end.
 }
 
+// TODO: File
 export class File implements FileHandle{
 
   private data : Uint8Array;
@@ -47,43 +49,62 @@ export class File implements FileHandle{
     this.mode = FileMode.Read;
   }
 
-  read(arg: number): Uint8Array {
-    throw new Error("Method not implemented.");
+  read(num: number): Uint8Array {
+    if(this.data.byteLength < num) 
+      throw new EOFError ("Cannot read past file byte size");
+    
+    const bytes2Read = Math.min(num, this.data.length - this.pos);
+    const result = this.data.slice(this.pos, this.pos + bytes2Read);
+    this.pos += bytes2Read; //adjusts position in array;
+    return result;
   }
-  write(arg: Uint8Array): void {
-    throw new Error("Method not implemented.");
+  write(data: Uint8Array): void {
+    if (this.mode == FileMode.Read)
+      throw new IllegalStateError("Cannot write while in read mode");
+
+    const data3 = new Uint8Array(this.data.length + data.length)
   }
-  truncate(arg: File): void {
-   throw new Error("Method not implemented.");
+  truncate(): void {
+   if (this.mode != FileMode.Write && this.mode != FileMode.WriteAppend)
+    throw new IllegalStateError("Can only truncate in write mode");
+   else
+    this.setLength(this.pos);
+
   }
-  seek(arg: number): void {
-    throw new Error("Method not implemented.");
+  seek(num: number): void {
+    if (num > this.getLength() || num < 0)
+      throw new RangeError("Cannot seek outside of file bounds");
+    else 
+      this.pos = num;
   }
 
   getLength(){
     return this.data.length;
   }
 
-  setLength(arg: number) {
-    if (arg < 0)
-      return new RangeError, "length cannot be negative";
+  setLength(num: number) {
+    if (num < 0)
+      throw new RangeError ("length cannot be negative"); //duh
 
-
-    
-
-
+    const data2 = new Uint8Array(num);
+    data2.set(this.data.slice(0, num)); //slices chunk (0 -> arg) out of data and sets to data2
+    this.data = data2;
   }
 
   clear(){
-
+    this.setLength(0);
   }
 
-  open(arg: number) {
+  open(num: number) {
+    if (this.mode == 1)
+      this.clear(); 
 
+    if (this.mode == 4 || this.mode == 8 || this.mode == 16) 
+      this.pos = this.data.length;
+    else 
+      this.pos = 0;
 
-    return new File;
+    return this; 
   }
-
-
 }
-// TODO: File
+
